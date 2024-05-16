b7fd35920acbc23592684dfa5bd87339feff0259
Ghiffary Rifqialdi <grifqialdi@gmail.com>
2024-05-16 13:10:55 +0700

    v2: remove backend_forecast flask implementation

--- backend_forecast/app/home/view.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from api.routes import Category, Route
-
-
-api_home = Blueprint(Category.HOME, __name__)
-
-@api_home.route(Route.DEFAULT, methods=['GET'])
-def home():
-    return "Forecaster is ready!"
--- backend_forecast/gunicorn_starter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-gunicorn --chdir app main:app -b 0.0.0.0:8000 --timeout 1000
