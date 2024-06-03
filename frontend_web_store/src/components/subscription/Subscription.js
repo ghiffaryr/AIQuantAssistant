062b344952619ba63b8a8f74d34189d98b139363
Ghiffary Rifqialdi <grifqialdi@gmail.com>
2024-06-03 13:46:30 +0700

    v2.9: update subscription page

--- frontend_web_store/src/components/subscription/Subscription.js
+++ frontend_web_store/src/components/subscription/Subscription.js
@@ -35,7 +35,8 @@ export default function Subscription({
   const [validated, setValidated] = useState(false);
   const [showPredictToast, setShowPredictToast] = useState(false);
   const [errorPredict, setErrorPredict] = useState({});
-  const [prediction, setPrediction] = useState({});
+  const [forecastPrediction, setForecastPrediction] = useState({});
+  const [textPrediction, setTextPrediction] = useState("");
   const [loading, setLoading] = useState(false);
 
   function handleChange(e) {
@@ -69,7 +70,7 @@ export default function Subscription({
     getProductCategory(productCategoryCode);
   }, [productCategoryCode]);
 
-  const handlePredict = async (
+  const handleForecastPredict = async (
     stockCode,
     trainingWindow,
     forecastingHorizon
@@ -88,7 +89,7 @@ export default function Subscription({
           forecastingHorizon: forecastingHorizon,
         }
       );
-      setPrediction(data);
+      setForecastPrediction(data);
       setErrorPredict({});
       setShowPredictToast(true);
     } catch (error) {
@@ -100,17 +101,73 @@ export default function Subscription({
     setLoading(false);
   };
 
-  const handleSubmitPredict = async (e) => {
+  const handleSubmitForecastPredict = async (e) => {
     const form = e.currentTarget;
 
     setValidated(true);
     e.preventDefault();
     if (form.checkValidity()) {
-      await handlePredict(
+      await handleForecastPredict(
         e.target[0].value,
         e.target[1].value,
         e.target[2].value
       );
+    }
+  };
+
+  function numberToPercentage(number) {
+    if (typeof number !== "number") {
+      throw new Error("Input must be a number");
+    }
+
+    const percentage = (number * 100).toFixed(2);
+    return `${percentage}%`;
+  }
+
+  const handleTextPredict = async (input) => {
+    setLoading(true);
+    try {
+      axios.defaults.headers.common = {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        "Access-Control-Allow-Origin": "*",
+      };
+      let { status, data } = await axios.post(
+        `${API}/category/${productCategoryCode}/predict`,
+        {
+          input: input,
+        }
+      );
+      if (productCategoryCode === "text_summary") {
+        setTextPrediction(data);
+      } else if (
+        productCategoryCode === "text_sentiment" ||
+        productCategoryCode === "text_topic"
+      ) {
+        setTextPrediction(
+          `${numberToPercentage(Number(data[0].score)).toString()} ${
+            data[0].label
+          }`
+        );
+      }
+
+      setErrorPredict({});
+      setShowPredictToast(true);
+    } catch (error) {
+      for (let errorObject of error.response.data.errors) {
+        setErrorPredict(errorObject);
+        setShowPredictToast(true);
+      }
+    }
+    setLoading(false);
+  };
+
+  const handleSubmitTextPredict = async (e) => {
+    const form = e.currentTarget;
+
+    setValidated(true);
+    e.preventDefault();
+    if (form.checkValidity()) {
+      await handleTextPredict(e.target[0].value);
     }
   };
 
@@ -137,123 +194,200 @@ export default function Subscription({
                 </small>
               </div>
             </div>
-            <div className="col col-8">
-              <div className="card-body d-flex flex-column justify-content-between">
-                <div className="card-description">
-                  <h5 className="card-title">
-                    {productCategory.productCategoryName}
-                  </h5>
-                  <h6 className="card-subtitle mb-2 text-muted">
-                    {productCategory.productCategoryCode}
-                  </h6>
-                  <p className="card-text">
-                    {productCategory.productCategoryDescription}
-                  </p>
-                </div>
-                <div className="card-form w-100 align-self-center mt-3">
-                  <Form
-                    className="cart-form row row-cols-1 row-cols-lg-3 g-4 justify-content-center"
-                    noValidate
-                    validated={validated}
-                    onSubmit={handleSubmitPredict}
-                  >
-                    <div className="col">
-                      <FloatingLabel
-                        controlId="floatingInput"
-                        label="Stock Code"
-                        className="mb-3"
-                      >
-                        <Form.Control
-                          type="text"
-                          name="stockCode"
-                          value={inputs.stockCode}
-                          onChange={handleChange}
-                          onWheel={(e) => e.target.blur()}
-                          placeholder="Stock Code"
-                          pattern="^(?!\s*$).+"
-                          required
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          Stock code cannot be blank.
-                        </Form.Control.Feedback>
-                      </FloatingLabel>
-                    </div>
-
-                    <div className="col">
-                      <FloatingLabel
-                        controlId="floatingInput"
-                        label="Training Window (month)"
-                        className="mb-3"
-                      >
-                        <Form.Control
-                          type="number"
-                          name="trainingWindow"
-                          value={inputs.trainingWindow}
-                          onChange={handleChange}
-                          onWheel={(e) => e.target.blur()}
-                          placeholder="Training Window (month)"
-                          min={1}
-                          required
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          Minimum training window is 1.
-                        </Form.Control.Feedback>
-                      </FloatingLabel>
-                    </div>
-                    <div className="col">
-                      <FloatingLabel
-                        controlId="floatingInput"
-                        label="Forecasting Horizon (month)"
-                        className="mb-3"
-                      >
-                        <Form.Control
-                          type="number"
-                          name="forecastingHorizon"
-                          value={inputs.forecastingHorizon}
-                          onChange={handleChange}
-                          onWheel={(e) => e.target.blur()}
-                          placeholder="Forecasting Horizon (month)"
-                          min={1}
-                          required
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          Minimum forecasting horizon is 1.
-                        </Form.Control.Feedback>
-                      </FloatingLabel>
-                    </div>
-                    {loading ? (
-                      <div className="col text-center">
-                        <div
-                          className="spinner-border text-primary"
-                          role="status"
-                          style={{
-                            width: "2rem",
-                            height: "2rem",
-                            borderWidth: "0.25rem",
-                          }}
-                        />
-                      </div>
-                    ) : (
-                      <div className="col text-center ">
-                        <Button
-                          className="w-100"
-                          type="submit"
-                          variant="outline-primary"
+            {productCategory.productCategoryCode.startsWith("forecast") && (
+              <div className="col col-8">
+                <div className="card-body d-flex flex-column justify-content-between">
+                  <div className="card-description">
+                    <h5 className="card-title">
+                      {productCategory.productCategoryName}
+                    </h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {productCategory.productCategoryCode}
+                    </h6>
+                    <p className="card-text">
+                      {productCategory.productCategoryDescription}
+                    </p>
+                  </div>
+                  <div className="card-form w-100 align-self-center mt-3">
+                    <Form
+                      className="cart-form row row-cols-1 row-cols-lg-3 g-4 justify-content-center"
+                      noValidate
+                      validated={validated}
+                      onSubmit={handleSubmitForecastPredict}
+                    >
+                      <div className="col">
+                        <FloatingLabel
+                          controlId="floatingInput"
+                          label="Stock Code"
+                          className="mb-3"
                         >
-                          Predict
-                        </Button>
+                          <Form.Control
+                            type="text"
+                            name="stockCode"
+                            value={inputs.stockCode}
+                            onChange={handleChange}
+                            onWheel={(e) => e.target.blur()}
+                            placeholder="Stock Code"
+                            pattern="^(?!\s*$).+"
+                            required
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            Stock code cannot be blank.
+                          </Form.Control.Feedback>
+                        </FloatingLabel>
                       </div>
-                    )}
-                  </Form>
+
+                      <div className="col">
+                        <FloatingLabel
+                          controlId="floatingInput"
+                          label="Training Window (month)"
+                          className="mb-3"
+                        >
+                          <Form.Control
+                            type="number"
+                            name="trainingWindow"
+                            value={inputs.trainingWindow}
+                            onChange={handleChange}
+                            onWheel={(e) => e.target.blur()}
+                            placeholder="Training Window (month)"
+                            min={1}
+                            required
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            Minimum training window is 1.
+                          </Form.Control.Feedback>
+                        </FloatingLabel>
+                      </div>
+                      <div className="col">
+                        <FloatingLabel
+                          controlId="floatingInput"
+                          label="Forecasting Horizon (month)"
+                          className="mb-3"
+                        >
+                          <Form.Control
+                            type="number"
+                            name="forecastingHorizon"
+                            value={inputs.forecastingHorizon}
+                            onChange={handleChange}
+                            onWheel={(e) => e.target.blur()}
+                            placeholder="Forecasting Horizon (month)"
+                            min={1}
+                            required
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            Minimum forecasting horizon is 1.
+                          </Form.Control.Feedback>
+                        </FloatingLabel>
+                      </div>
+                      {loading ? (
+                        <div className="col text-center">
+                          <div
+                            className="spinner-border text-primary"
+                            role="status"
+                            style={{
+                              width: "2rem",
+                              height: "2rem",
+                              borderWidth: "0.25rem",
+                            }}
+                          />
+                        </div>
+                      ) : (
+                        <div className="col text-center ">
+                          <Button
+                            className="w-100"
+                            type="submit"
+                            variant="outline-primary"
+                          >
+                            Predict
+                          </Button>
+                        </div>
+                      )}
+                    </Form>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
+            {productCategory.productCategoryCode.startsWith("text") && (
+              <div className="col col-8">
+                <div className="card-body d-flex flex-column justify-content-between">
+                  <div className="card-description">
+                    <h5 className="card-title">
+                      {productCategory.productCategoryName}
+                    </h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {productCategoryCode}
+                    </h6>
+                    <p className="card-text">
+                      {productCategory.productCategoryDescription}
+                    </p>
+                  </div>
+                  <div className="card-form w-100 align-self-center mt-3">
+                    <Form
+                      className="cart-form row row-cols-1 row-cols-lg-1 g-4 justify-content-center"
+                      noValidate
+                      validated={validated}
+                      onSubmit={handleSubmitTextPredict}
+                    >
+                      <div className="col">
+                        <FloatingLabel
+                          controlId="floatingInput"
+                          label="Input"
+                          className="mb-3"
+                        >
+                          <Form.Control
+                            as="textarea"
+                            name="input"
+                            value={inputs.input}
+                            onChange={handleChange}
+                            onWheel={(e) => e.target.blur()}
+                            placeholder="Input"
+                            required
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            Input cannot be blank.
+                          </Form.Control.Feedback>
+                        </FloatingLabel>
+                      </div>
+                      {loading ? (
+                        <div className="col text-center">
+                          <div
+                            className="spinner-border text-primary"
+                            role="status"
+                            style={{
+                              width: "2rem",
+                              height: "2rem",
+                              borderWidth: "0.25rem",
+                            }}
+                          />
+                        </div>
+                      ) : (
+                        <div className="col text-center ">
+                          <Button
+                            className="w-100"
+                            type="submit"
+                            variant="outline-primary"
+                          >
+                            Predict
+                          </Button>
+                        </div>
+                      )}
+                      {textPrediction && (
+                        <div className="container">
+                          <h3 className="mt-4">Summary</h3>
+                          <p className="text-black">{textPrediction}</p>
+                        </div>
+                      )}
+                    </Form>
+                  </div>
+                </div>
+              </div>
+            )}
             <div className="col col-12">
-              {Object.keys(prediction).length > 0 && (
+              {Object.keys(forecastPrediction).length > 0 && (
                 <Plot
                   className="w-100"
-                  data={prediction.data}
-                  layout={prediction.layout}
+                  data={forecastPrediction.data}
+                  layout={forecastPrediction.layout}
                   config={{ responsive: true }}
                 />
               )}
