b996c278b37c6a810287000e73c246fba2db8691
renardelyon <renard.elyon.r@gmail.com>
2024-06-07 14:14:13 +0700

    Install and configure prettier

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,10 +5,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'react-hooks', 'jsx-a11y', 'prettier' ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
